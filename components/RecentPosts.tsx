b91fc5751af97d7f96e9f8e24090e211edacfe64
Aditya Restu Hudayana <155047795+adityarestuhudayana@users.noreply.github.com>
2026-03-30 19:28:02 +0700

    Feat (#27)
    * feat: add check spam folder on emails
    
    * feat: privacy policy and term of service pages
    
    * feat: complete data in /about-us
    
    * fix: key prop issue

--- components/RecentPosts.tsx
+++ components/RecentPosts.tsx
@@ -51,9 +51,9 @@ export function RecentPosts() {
             const avatarLetter = msg.recipient_name?.[0] || "?";
 
             return (
-              <Link href={`/explore/${msg.id}`}>
+              <Link key={msg.id} href={`/explore/${msg.id}`}>
                 <Card
-                  key={msg.id}
+
                   className="group bg-white rounded-3xl p-1 shadow-lg border-[6px] border-white transition-all hover:-translate-y-2 cursor-pointer"
                 >
                   <CardContent className="flex flex-col flex-1 min-h-[260px] rounded-2xl p-6 space-y-6">
